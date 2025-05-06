193ac43f9544dcc018463b8af499bba2a8b5bf22
ada3012 <mendozahernandezadair@gmail.com>
2025-05-05 21:26:44 -0600

    fuentes

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,11 @@ class Menu extends HTMLElement {
 
     shadow.innerHTML = `
       <style>
+      @import url("../css/fonts.css");
+
+      li{
+      font-family:"Montserrat-Medium";
+      }
 menu {
     background-color: #2c3e50;
     padding: 10px 0;
@@ -34,10 +39,10 @@ menu ul li a {
 }
 
 menu ul li a:hover {
-    background-color: #e74c3c;
+    background-color:rgb(100, 134, 219);
     color: #fff;
     transform: scale(1.1);
-    box-shadow: 0 4px 10px rgba(231, 76, 60, 0.4);
+    box-shadow: 0 4px 10px rgba(73, 227, 238, 0.4);
 }
 
 menu ul li img {
@@ -45,6 +50,7 @@ menu ul li img {
     height: auto;
     margin-right: 20px;
 }
+
       </style>
       <slot>
           <menu>
@@ -52,12 +58,12 @@ menu ul li img {
           <li>
             <img src="multimedia/logo_1er.png" class="primer_logo" alt="logo" />
           </li>
-          <li><a href="index.html">Inicio</a></li>
-          <li><a href="tu_casa.html">Tu casa</a></li>
-          <li><a href="servicios.html">Camino a casa</a></li>
-          <li><a href="contacto.html">Mis primeros pasos</a></li>
-          <li><a href="servicios.html">Predicas</a></li>
-          <li><a href="contacto.html">Contacto</a></li>
+          <li><a href="index.html">INICIO</a></li>
+          <li><a href="tu_casa.html">TU CASA</a></li>
+          <li><a href="servicios.html">CAMINO A CASA</a></li>
+          <li><a href="contacto.html">MIS PRIMEROS PASOS</a></li>
+          <li><a href="servicios.html">PREDICAS</a></li>
+          <li><a href="contacto.html">CONTACTO</a></li>
         </ul>
       </menu>
       </slot>
